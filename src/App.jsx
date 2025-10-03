89c5e2652f948e8bfaeb19a468509fd8b6d0a943
Jithu SJ <jithusj169@gmail.com>
2025-10-03 15:30:38 +0530

    Shoe card Created

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Footer,Hero,PopularProducts,Subscribe,
   const App = () => (
   <main className="relative">
     <Nav />
-    <section className="xl:padding-1
+    <section className="xl:padding-l
     wide:padding-r padding-b">
       <Hero />
     </section>
