db3ba1ed30ba8a90f81573ff1665e354dee0039f
Jithu SJ <jithusj169@gmail.com>
2025-10-04 14:33:01 +0530

    Created SuperQuality Products

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,9 +7,9 @@ const Button = ({label,iconURL}) => {
    rounded-full text-white
    border-coral-red">
     {label}
-    <img src={iconURL}
+    {iconURL && <img src={iconURL}
     alt="arrow right icon"
-    className="ml-2 rounded-full w-5 h-5"/>
+    className="ml-2 rounded-full w-5 h-5"/>}
    </button>
   )
 }
