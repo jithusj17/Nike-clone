667bc4b01a6b0f0c79b31a7448907b64ca5f3ac1
Jithu SJ <jithusj169@gmail.com>
2025-10-04 11:54:17 +0530

    Created Popular Products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,4 +1,5 @@
-
+import PopularProductCard from '../components/PopularProductCard'
+import {products} from '../constants'
 const PopularProducts = () => {
   return (
     <section id="products"
@@ -9,12 +10,20 @@ const PopularProducts = () => {
         <h2 className="text-4xl font-palanquin font-bold"> Our 
           <span className="text-coral-red">  Popular
           </span> Products </h2>
-        <p>Experience top-notch quality
+        <p className='lg:max-w-lg font-montserrat text-slate-gray'>Experience top-notch quality
           and style with our sought-after
           selections.Discover a world of 
           comfort ,design,and value
         </p>
+      </div>
+      <div className='mt-16 grid lg:grid-cols-4
+      md:grid-cols-3 sm:grid-cols-2 grid-cols-1
+      sm:gap-4 gap-14'>
+        {products.map((product)=>(
+          <PopularProductCard key={product.name} {...product}/>
+        ))
 
+        }
       </div>
 
     </section>
